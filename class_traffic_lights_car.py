935b6b2d51cc99efa5ab56fd7f2481b088ecab85
CHO1YJ <c5898559631@gmail.com>
2022-07-31 23:10:53 +0900

    Add a file to implement car traffic lights using class in Python

---
+++ class_traffic_lights_car.py
@@ -0,0 +1,18 @@
+# 차량용 신호등 클래스
+class TrafficLightsCar:
+    # 신호등 필드; 신호등의 동작에 필요한 변수 정의
+    __t = 0
+    
+    # 생성자
+    def __init__(self):
+        self.__t = 0
+        
+    # 신호등 메서드; 신호등의 동작 기능 정의
+    def to_do_addition():
+        pass
+    
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
